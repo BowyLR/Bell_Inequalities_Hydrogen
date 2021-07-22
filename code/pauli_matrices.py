bfab356382d5b28a3b4b040485045025a8cb4173
BowyLR <B.M.LaRiviere@student.tudelft.nl>
2021-07-22 15:31:44 +0200

    adds general Bell operator and brings more structure to the code

---
+++ code/pauli_matrices.py
@@ -0,0 +1,8 @@
+# importing python packages
+import numpy as np
+
+# Defining Pauli matrices and the identity matrix
+I = np.array([[1, 0], [0, 1]])
+X = np.array([[0, 1], [1, 0]])
+Y = np.array([[0, -1j], [1j, 0]])
+Z = np.array([[1, 0], [0, -1]])

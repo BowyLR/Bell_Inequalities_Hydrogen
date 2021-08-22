25fcded0d2a2e13173330a34b5d70173c32c25f9
BowyLR <bowylar1995@gmail.com>
2021-08-22 23:00:53 +0200

    makes it so the computations of the Hamiltonian is more automized and better integrated with the openfermionpyscf package

--- code/hamiltonian/fermionic_hamiltonian.py
+++ code/hamiltonian/fermionic_hamiltonian.py
@@ -1,39 +1,38 @@
 # Importing python packages
-import numpy as np
-from openfermion.ops import FermionOperator
+from openfermion import MolecularData, get_fermion_operator, FermionOperator
+from openfermionpyscf import run_pyscf
 
-def get_fermionic_hamiltonian(N=4):
-    # Let us define a molecular Hamiltonian that we can use to feed it to openfermion
-    h11 = h22 = -1.252477
-    h33 = h44 = -0.475934
-    v1221 = v2112 = 0.674493
-    v3443 = v4334 = 0.697397
-    v1331 = v2442 = v2332 = v2442 = v3113 = v1441 = v3223 = v4224 = 0.663472
-    v1313 = v2424 = v3241 = v3421 = v1423 = v1234 = 0.181287
+# Initializing the openfermionpyscf software
+run_scf = 1
+run_mp2 = 0
+run_cisd = 0
+run_ccsd = 0
+run_fci = 0
+delete_input = True
+delete_output = True
 
-    # Initializing single particle and Coulomb operators
-    h_single_and_coulomb = np.array([
-        [h11, v1221, v1331-v1313, v1441       ],
-        [0,   h22,   v2332,       v2442-v2424 ],
-        [0,   0,     h33,         v3443       ],
-        [0,   0,     0,           h44         ]
-    ]) 
 
-    # Initializing the Hamiltonian
-    hamiltonian_fermionic_op = 0
+def get_fermionic_hamiltonian(geometry, basis, multiplicity, charge):
 
-    # Adding the Coulom interactions to the Hamiltonian
-    for i in range(N):
-        # Adding single Hamiltonian terms
-        hamiltonian_fermionic_op += FermionOperator(str(i)+'^ '+str(i), h_single_and_coulomb[i, i])
+    # Defining the molecule
+    description = str(charge)
+    mol = MolecularData(geometry, basis, multiplicity,
+                         charge, description)
 
-        # Adding coulomb terms
-        for j in range(i+1, N):
-            hamiltonian_fermionic_op += FermionOperator(str(j) + '^ ' + str(j) + ' ' + str(i) + '^ ' + str(i), h_single_and_coulomb[i, j])
-
-    # Adding the double excitation operators
-    hamiltonian_fermionic_op += (FermionOperator('0^ 1^ 3 2', v1234) + FermionOperator('2^ 3^ 1 0', v1234) +
-                FermionOperator('0^ 3^ 1 2', v1234) + FermionOperator('2^ 1^ 3 0', v1234)
+    # Running pyscf
+    molecule = run_pyscf(mol,
+        run_scf=run_scf,
+        run_mp2=run_mp2,
+        run_cisd=run_cisd,
+        run_ccsd=run_ccsd,
+        run_fci=run_fci
     )
 
-    return hamiltonian_fermionic_op
+    # Obtaining the fermionic Hamiltonian
+    molecular_hamiltonian = molecule.get_molecular_hamiltonian()
+    fermionic_hamiltonian = get_fermion_operator(molecular_hamiltonian)
+
+    # Removing the nuclear repuslion 
+    fermionic_hamiltonian += -FermionOperator('', fermionic_hamiltonian.terms[()])
+
+    return fermionic_hamiltonian
--- code/hamiltonian/hamiltonian.py
+++ code/hamiltonian/hamiltonian.py
@@ -1,5 +1,4 @@
 # Importing python packages
-import sys
 import numpy as np
 from openfermion.transforms import jordan_wigner, bravyi_kitaev
 
@@ -10,21 +9,21 @@ from pauli_matrices.pauli_matrices import I, X, Y, Z
 from hamiltonian.fermionic_hamiltonian import get_fermionic_hamiltonian
 from hamiltonian.matrix_hamiltonian import get_matrix_form
 
+
 class get_hamiltonian():
 
-    def __init__(self, qubit_transform = 'JW'):
+    def __init__(self, geometry, basis, multiplicity, charge, qubit_transform='JW'):
 
         # Implementing fail safe for unwanted models
         possible_qubit_transforms = ['JW', 'BK']
         if np.sum([(qubit_transform == j) for j in possible_qubit_transforms]) != 1:
 
-            sys.exit('retrieved '+qubit_transform+' but this name is incompatible. \nPossible qubit transformations are \'JW\' (Jordan-Wigner) and \'BK\' (Bravyi-Kitaev)')
-
-        # Defining the number of qubits
-        self.N = 4
+            raise ValueError('retrieved '+ qubit_transform +
+                            ' as an input, but this name is incompatible. ' + 
+                            '\nPossible qubit transformations are \'JW\' (Jordan-Wigner) and \'BK\' (Bravyi-Kitaev)')
 
         # Calculating the fermionic Hamiltonian
-        self.fermionic_hamiltonian = get_fermionic_hamiltonian(N=self.N)
+        self.fermionic_hamiltonian = get_fermionic_hamiltonian(geometry, basis, multiplicity, charge)
 
         # Obtaining the qubit Hamiltonian
         if qubit_transform == possible_qubit_transforms[0]:
@@ -33,11 +32,8 @@ class get_hamiltonian():
         elif qubit_transform == possible_qubit_transforms[1]:
             self.qubit_hamiltonian = bravyi_kitaev(self.fermionic_hamiltonian)
 
+        # Obtaining the number of qubits in the Hamiltonian
+        self.N = self.qubit_hamiltonian.many_body_order()
+
         # Obtaining the matrix form of the Hamiltonian
         self.matrix_form = get_matrix_form(self.N, self.qubit_hamiltonian)
-
-
-
-        
-
-
